45fdb69cd237f9bf3a44f3051e05b3c96ae6ee2d
Jack Mu <jackmu@umich.edu>
2024-12-09 20:32:06 -0800

    added main functionality for tiptap but need to make sure css exists for each type of html node

--- justfrands-frontend/src/components/TiptapEditor/TiptapEditor.tsx
+++ justfrands-frontend/src/components/TiptapEditor/TiptapEditor.tsx
@@ -2,11 +2,25 @@ import { useEditor, EditorContent, FloatingMenu, BubbleMenu } from '@tiptap/reac
 import Paragraph from '@tiptap/extension-paragraph';
 import Document from '@tiptap/extension-document';
 import Text from '@tiptap/extension-text';
+import Image from '@tiptap/extension-image';
+import Youtube from '@tiptap/extension-youtube';
+import BulletList from '@tiptap/extension-bullet-list';
+import ListItem from '@tiptap/extension-list-item';
+import CodeBlock from '@tiptap/extension-code-block';
+import Heading from '@tiptap/extension-heading';
+import StarterKit from '@tiptap/starter-kit';
 
 // define your extension array
-const extensions = [Document, Paragraph, Text]
+const extensions = [StarterKit, Document, Paragraph, Heading.configure({ levels: [1, 2, 3] }),
+	Text, Image, Youtube, BulletList, ListItem, CodeBlock];
 
-const content = '<p>Hello World!</p>'
+const content = `<p>Hello World!</p>
+	<h1>heading</h1>
+	<h2>heading2</h2>
+	<ul>
+		<li>1</li>
+		<li>2</li>
+	</ul>`
 
 const TiptapEditor = () => {
 	const editor = useEditor({
