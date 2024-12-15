79719e547854b281fb588967c7eabc0683210691
Jack Mu <jackmu@umich.edu>
2024-12-15 10:49:31 -0800

    have a floating bar and need to implement the bubble menu

--- justfrands-frontend/src/App.tsx
+++ justfrands-frontend/src/App.tsx
@@ -3,7 +3,7 @@ import EditorPage from './pages/EditorPage';
 
 function App() {
   return (
-    <div className='pt-40 pb-20 absolute top-0 left-0 w-screen flex flex-col justify-start items-center'>
+    <div className='pt-20 absolute top-0 left-0 w-screen flex flex-col justify-start items-center'>
       <EditorPage />
     </div>
   );
--- justfrands-frontend/src/components/TiptapEditor/TiptapEditor.tsx
+++ justfrands-frontend/src/components/TiptapEditor/TiptapEditor.tsx
@@ -9,31 +9,32 @@ import ListItem from '@tiptap/extension-list-item';
 import CodeBlock from '@tiptap/extension-code-block';
 import Heading from '@tiptap/extension-heading';
 import StarterKit from '@tiptap/starter-kit';
-import { TiptapFloatingMenu } from './TiptapFloatingMenu';
+import { TiptapMenu } from './TiptapMenu';
+import { TiptapBubbleMenu } from './TiptapBubbleMenu';
 
 // define your extension array
 const extensions = [StarterKit, Document, Paragraph, Heading.configure({ levels: [1, 2, 3] }),
 	Text, Image, Youtube, BulletList, ListItem, CodeBlock];
 
-const content = `<p>Hello World!</p>
-	<h1>heading</h1>
-	<h2>heading2</h2>
-	<ul>
-		<li>1</li>
-		<li>2</li>
-	</ul>`
+const content = `<h2>Start Typing to Begin</h2>`
+
 
 const TiptapEditor = () => {
 	const editor = useEditor({
 		extensions,
 		content,
-	})
+		editorProps: {
+			attributes: {
+				class: 'focus:outline-none',
+			},
+		}
+	});
 
 	return (
-		<div className={"w-full h-full bg-stone-100 text-left text-black p-4 rounded-lg"}>
-			<EditorContent editor={editor} />
-			<TiptapFloatingMenu editor={editor}></TiptapFloatingMenu>
-			{/* <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu> */}
+		<div className={"w-full h-full text-left text-black "}>
+			<TiptapMenu editor={editor}></TiptapMenu>
+			<EditorContent className='bg-stone-100 p-4 rounded-lg mt-8' editor={editor} />
+			<TiptapBubbleMenu editor={editor} />
 		</div>
 	);
 }
