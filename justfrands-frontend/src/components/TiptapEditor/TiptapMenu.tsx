3cc132bf070aff9624ea47da8cd678b51e15b961
Jack Mu <jackmu@umich.edu>
2025-01-04 10:27:41 -0500

    using gap instead of space to make the first element align; removed some colors from side bar

--- justfrands-frontend/src/components/TiptapEditor/TiptapMenu.tsx
+++ justfrands-frontend/src/components/TiptapEditor/TiptapMenu.tsx
@@ -32,7 +32,7 @@ export const TiptapMenu = ({ editor }: { editor: Editor | null }) => {
 				 sticky top-10 border-2 shadow-xl flex'>
 			{editor != null &&
 				<div className='flex flex-row items-center justify-between w-full'>
-					<div className='flex space-x-2 flex-wrap space-y-2'>
+					<div className='flex space-x-2 flex-wrap gap-y-1'>
 						<button onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
 							className={editor.isActive('heading', { level: 1 }) ?
 								'font-bold bg-purple-100 border-2 border-purple-700 text-sm' :
@@ -74,18 +74,18 @@ export const TiptapMenu = ({ editor }: { editor: Editor | null }) => {
 							className='border-2 border-stone-300 text-sm'> &lt;img&gt;
 						</button>
 					</div>
-					<div className='flex flex-col w-32 space-y-1 border-l-2 px-2 ml-2 border-stone-100'>
+					<div className='flex flex-col w-24 space-y-0 border-l-2 px-2 ml-2 border-stone-100'>
 						<button onClick={sendDocument}
-							className='border-2 border-stone-300 p-1 bg-amber-100 hover:bg-amber-300 hover:-translate-y-0.5 text-xs font-bold'>
+							className='py-1 px-0 rounded-none border-b-1 border-b-stone-200 bg-inherit hover:-translate-y-0.5 text-xs font-bold'>
 							Send Page
 						</button>
 						<button onClick={sendDocument}
-							className='border-2 border-stone-300 p-1 bg-orange-100 hover:bg-orange-300 hover:-translate-y-0.5 text-xs font-bold'>
+							className='py-1 px-0 rounded-none border-b-1 border-b-stone-200 bg-inherit hover:-translate-y-0.5 text-xs font-bold'>
 							Save Page
 						</button>
 
 						<button onClick={redirectToAccount}
-							className='border-2 border-stone-300 p-1 bg-green-100 hover:bg-green-300 hover:-translate-y-0.5 text-xs font-bold'>
+							className='py-1 px-0 rounded-none border-b-1 border-b-stone-200 bg-inherit hover:-translate-y-0.5 text-xs font-bold'>
 							Account
 						</button>
 					</div>
